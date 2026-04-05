9d9b6b41202b8468111e004db57b195de95f8715
veddd27 <ved2702ambulkar@gmail.com>
2026-04-05 23:38:18 +0530

    Added Work register controller

---
+++ src/routes/user.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
+
+const router = Router()
+
+router.route("/api/v1/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverIamge",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
+
+export default router
