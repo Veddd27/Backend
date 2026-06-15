1775aa65cfa7d18039b4d633997ec61088ddda3c
Ved Ambulkar <ved2702ambulkar@gmail.com>
2026-06-15 23:56:51 +0530

    Added user controllers, routes, middlewares, schemas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { loginUser } from "../controllers/user.controller.js";
+import { logoutUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword } from "../controllers/user.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
+import { updateAccountDetails } from "../controllers/user.controller.js";
+import { updateUserAvatar } from "../controllers/user.controller.js";
+import { updateUserCoverImage } from "../controllers/user.controller.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
+import { getUserChannelProfile } from "../controllers/user.controller.js";
+import { getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -16,5 +27,16 @@ router.route("/api/v1/register").post(
         }
     ]),
     registerUser)
+
+    router.route("/api/v1/login").post(loginUser)
+    router.route("/api/v1/logout").post(verifyJWT, logoutUser)
+    router.route("/api/v1/refresh-token").post(refreshAccessToken)
+    router.route("/api/v1/change-password").post(verifyJWT, changeCurrentPassword)
+    router.route("/api/v1/current-user").get(verifyJWT, getCurrentUser)
+    router.route("/api/v1/update-account").patch(verifyJWT, updateAccountDetails)
+    router.route("/api/v1/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/api/v1/cover-image").put(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+    router.route("/api/v1/c/:username").get(getUserChannelProfile)
+    router.route("/api/v1/history").get(verifyJWT, getWatchHistory)
 
 export default router
