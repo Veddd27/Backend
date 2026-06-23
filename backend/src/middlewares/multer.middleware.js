d5b74d55c26a41cfdb0e6a5d2ce5f22b62b53807
Ved Ambulkar <ved2702ambulkar@gmail.com>
2026-06-23 16:00:00 +0530

    chore: use os.tmpdir() for serverless compatibility

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,15 +1,17 @@
 import multer from 'multer'
+import os from 'os'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    cb(null, os.tmpdir())
   },
   filename: function (req, file, cb) {
-    
-    cb(null, Date.now() + '' + file.originalname)
+    cb(null, Date.now() + '-' + file.originalname)
   }
 })
 
 export const upload = multer({ 
     storage, 
 })
+
+
